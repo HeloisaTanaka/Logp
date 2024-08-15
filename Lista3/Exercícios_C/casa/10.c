6d7025096b78be94b074732c49213f34a4963b4c
Heloisa Tanaka <helo.l.tanaka@gmail.com>
2024-08-15 20:28:57 -0300

    calculadora

--- Lista3/Exercícios_C/casa/10.c
+++ Lista3/Exercícios_C/casa/10.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 int main()
 {
-    char s;
-    int a, b;
-    printf("Digite o símbolo da operação, caso queira sair do programa, digite 'S': ");
-    scanf("%c", &s);
-    while(s="+" || s="-" || s="*" || s="/" || s="s" || s="S")
+    char s='+';
+    float a, b;
+    while (s!='s' || s!='S'){ 
+        printf("Digite o símbolo da operação, caso queira sair do programa, digite 'S':");
+        scanf (" %c", &s); 
+        getchar();
+        if (s=='s' || s=='S'){
+            printf("Programa encerrado");
+            break;
+        }
+        else if (s=='+' || s=='-' || s=='*' || s=='/'){
+            printf("Digite a variável a: ");
+            scanf("%f", &a);
+            printf("Digite a variável b: ");
+            scanf("%f", &b);
+            if (s=='+'){
+                printf("%.4f\n", a+b);
+            }
+            else if (s=='-'){
+                printf("%.4f\n", a-b);
+            }
+            else if (s=='*'){
+                printf("%.4f\n", a*b);
+            }
+            else {
+                printf("%.4f\n", a/b);
+            }
+        }
+        else {
+            printf("Operação inválida\n");
+        }
+    }
+    return 0;
 }
